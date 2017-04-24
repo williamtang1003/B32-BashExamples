97329c4fecce24a96ae3c91b398bf572a5103cef
William Tang <williamtang903@gmail.com>
2017-04-24 17:56:27 -0400

    added 04-condition.sh

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+note=60
+
+read -p "note: " note
+
+# syntaxe
+if test $note -lt 60
+then 
+	echo "you failed!"
+elif test $note -eq 60
+then
+	echo "ouf! C'est juste!"
+else
+	echo "Alright!"
+fi
 
 
+# pour un case
+read -p "Entrez une lettre: " lettre
+case $lettre in 
+	[[:lower:]])
+		echo "lettre en minuscule"
+		;;
+	*) # autres
+		echo "Majuscule"
+		;;
+esac # ferme le case
 
 
-
-
-
+if test $lettre!="a"
+then
+	echo "la lettre n'est pas a"
+fi
