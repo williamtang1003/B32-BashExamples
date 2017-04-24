b218c4bc02028a953fb9b6f3da3a7c71e50b5ddf
William Tang <williamtang903@gmail.com>
2017-04-24 17:33:48 -0400

    Done with hello world!

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo "salut"
+#exit
+echo "toi"
+
+echo -n "Vive"
+echo -e " Linux \n"
+echo -n "                       "
+echo -e "1\n2\n3"
+
+read -p "Quel âge avez-vous? " age
+echo $age "ans! Vous paraissez jeune!"
