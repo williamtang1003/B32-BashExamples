c127df36c836facc8c2333847aa0c6ae586dcd4c
William Tang <williamtang903@gmail.com>
2017-04-24 17:45:56 -0400

    added 03-calculs.sh

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# syntaxe pour les calculs et pas d'espace
+echo $((10+10))
+
+# invalide
+echo 10+10
+
+read -p "Nombre 1 : " nb1
+read -p "Nombre 2 : " nb2
+
+let resultat=$nb1+$nb2
+echo $resultat
