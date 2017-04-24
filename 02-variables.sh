8c5bc47de6830d2e3f3cdf447bd0867a8d666ce5
William Tang <williamtang903@gmail.com>
2017-04-24 17:41:18 -0400

    modified 02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# Ne pas mettre d'espace!!!
+prenom=William
+echo $prenom
+
+# m'assurer que c'est un int
+declare -i age=35
+declare -r age2=33 # Devient en lecture seule
+
+# on met des guillemets lorsqu'il y a des espaces
+cours="Cours de Linux"
+
+# affichage
+echo $age
+echo $age2
+echo $cours
+
+echo Le programme $0 a pris le paramètre $1
