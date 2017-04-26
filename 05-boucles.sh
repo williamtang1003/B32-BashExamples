4a8f0b90e297d95aa48f642da39d37a9a004c4a9
William Tang <williamtang903@gmail.com>
2017-04-26 14:55:54 -0400

    3 sortes de boucles dans 05-boucles.sh

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+# les espaces sont importants
+for ((i=0;i<5;i++))
+do 
+	echo $i
+done
+
+resultat=o
+
+while test $resultat = "o"
+do
+	read -p "Entrez une lettre : " resultat
+done
+
+# différence entre ls et maListe=`ls`
+# n'importeq quelle fonction peut se trouver dans `` 
+maListe=`ls`
+
+for element in $maListe
+do
+	echo $element
+done
