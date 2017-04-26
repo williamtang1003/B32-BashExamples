7e11b7abdbca38bef6425b86a0b5be37d8abde83
William Tang <williamtang903@gmail.com>
2017-04-26 15:11:19 -0400

    les expressions régulières 08-regexp.sh

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+#expressions régulières
+
+echo -n "Lettre(s):"
+read lettre
+
+#équivalent
+# read -p "Lettre(s): " lettre
+
+
+#équivalent de if test
+#expressions régulières
+# +: au moins 1. 
+# $: se termine par
+# \s: espace
+if [[ $lettre =~ ^[0-9]+$ ]]
+then
+	echo "C'est un nombre"
+fi
+
+if [[ $lettre =~ ^[a-zA-Z]+$ ]]
+then
+	echo "C'est un mot"
+fi
