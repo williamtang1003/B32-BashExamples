9856eaf939988bc01e4d7d160597f5953080e7c1
William Tang <williamtang903@gmail.com>
2017-04-26 15:16:23 -0400

    tests sur les fichiers dans 09-fichiers.sh

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# voir si le fichier existe avec -e (fichier) et -d (pour répertoire)
+# $# = nombre de paramètres passés au programme, excluant le nom du programme
+# -ge = greater or equal to
+
+
+if test $# -ge 1
+then
+	if test -e $1
+	then
+		echo "Il existe"
+	fi
+fi
