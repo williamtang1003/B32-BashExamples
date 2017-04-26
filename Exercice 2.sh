22fb7d27eb3775ed972f1f47a910c3d99f8d78d2
William Tang <williamtang903@gmail.com>
2017-04-26 15:54:39 -0400

    exercice 2: complété!

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+menu(){
+	echo "a- Chercher un mot dans le dictionnaire\n"
+	echo "b- ne rien faire...\n"
+	echo "q- quitter le programme"
+}
+
+# exécution
+clear
+
+menu 
+read lettre
+
+while test $lettre != q
+do
+	clear
+	menu 
+	read lettre
+done
